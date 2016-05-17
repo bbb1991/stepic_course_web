2eb048e3e91c058a759cc9c2a42a3ea901ff0666
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2016-05-18 01:05:03 +0600

    A little refactored. Removed Gunicorn conf file (we will run with command line)

--- initialize.sh
+++ initialize.sh
@@ -6,38 +6,31 @@ sudo apt-get install nginx
 
 # update Django
 sudo pip install --upgrade django
+
+# Run NGINX
 sudo nginx
+
 # creating project structure
 mkdir -p /home/box/web/etc/
 mkdir -p /home/box/web/uploads/
 mkdir -p /home/box/web/public/css/
 mkdir -p /home/box/web/public/img/
 mkdir -p /home/box/web/public/js/
+
 # Configure NGINX
 sudo rm -rf /etc/nginx/nginx.conf
 cp ./conf/nginx.conf /home/box/web/etc
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 
 # Configure Gunicorn and echo WSGI server
-cp ./files/hello.py /home/box/web/
-cp ./conf/hello.py /home/box/web/etc/
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-
-#gunicorn -c /etc/gunicorn.d/hello.py -b 0.0.0.0:8080 hello:app &
+#cp ./files/hello.py /home/box/web/
+#cp ./conf/hello.py /home/box/web/etc/
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 cp -r /home/box/stepic_course_web/ask /home/box/web
 cd /home/box/web/ask
+
+# Deploying echo server and Django project
+gunicorn hello:app & --bind 0.0.0.0:8000 &
 gunicorn ask.wsgi:application --bind 0.0.0.0:8000 &
 
-
-#ln -sf ~/web/stepic_course_web/ask ~/web/ask
-#django-admin startproject ask
-#cd ask
-#python manage.py startapp qa
-#echo -e """
-#from django.http import response
-
-#def test(response, *args, **kwargs):
-#    return HttpResponse('OK')
-
-#$$#$""" >> /home/box/web/ask/qa/views.py

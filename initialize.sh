e2bba65795d57cdacfe6f1325a1720c0392eb12c
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2016-05-17 23:55:14 +0600

    Add fix files

--- initialize.sh
+++ initialize.sh
@@ -15,9 +15,20 @@ sudo rm -rf /etc/nginx/nginx.conf
 cp ./conf/nginx.conf /home/box/web/etc
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 
-# COnfigure Gunicorn and echo WSGI server
+# Configure Gunicorn and echo WSGI server
 cp ./files/hello.py /home/box/web/
 cp ./conf/hello.py /home/box/web/etc/
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 gunicorn -c /etc/gunicorn.d/hello.py -b 0.0.0.0:8080 hello.py &
+cd /home/box/web
+django-admin startproject ask
+cd ask
+python manage.py startapp qa
+echo -e """
+from django.http import response
+
+def test(response, *args, **kwargs):
+    return HttpResponse('OK')
+
+""" >> /home/box/web/ask/qa/views.py
